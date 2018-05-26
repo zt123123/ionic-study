eb603acf95255bb8c125c5ec4c9b76637bde2e4f
zt123123 <249812928@qq.com>
2018-05-27 03:28:16 +0800

    test

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+	public items:Array<any>;
+  constructor(
+  	public navCtrl: NavController,
+  	public http: HttpClient
+  	) {
 
-  constructor(public navCtrl: NavController) {
-
+	this.request();
   }
+
+itemSelected(index){
+	console.log(index);
+}
+
+request(){
+let api = "http://localhost:3000/api/terminal";
+    this.http.post(api,{}).subscribe(response => {
+      this.items =	response['data'].experience_data;
+    });
+}
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+	private msg2:string;
+  constructor(
+  	public navCtrl: NavController,
+  	public alertCtrl: AlertController
+  	) {
+	this.msg2="hello ionic"
   }
+
+	tapEvent(event){
+		let alert = this.alertCtrl.create({
+		     title: '提示',
+		     subTitle: '老朋友',
+		     buttons: [{
+		     	text: '确定',
+		          handler: data => {
+		            console.log('确定 clicked');
+		          }
+      },{
+      	text: '取消',
+      	     handler: data => {
+      	       console.log('取消 clicked');
+      	     }
+      	
+      }]
+		   });
+		   alert.present();
+	}
 
 }
